98273ad57486c2c3861709641f44e99e5aa0f54a
Dean Longmire <deanlongmire100@gmail.com>
2023-12-15 21:17:45 -0500

    Generate a simple console log template

--- templates/starter/index.js
+++ templates/starter/index.js
@@ -1,12 +1,38 @@
-const fs = require("fs-extra")
-const ejs = require("ejs")
-const argv = require("yargs-parser")(process.argv.slice(2))
+const fs = require("fs-extra");
+const ejs = require("ejs");
+const argv = require("yargs-parser")(process.argv.slice(2));
+const path = require("path");
 
 const main = () => {
+    console.log("Generating template...");
     try {
-        console.log(argv)
+        const { _: leftovers, out, fn } = argv;
+
+        const data = {
+            fn,
+            leftovers,
+        };
+
+        const options = {};
+
+        if(!out || !fn) {
+            console.error("--out and --fn flag required");
+            process.exit(1);
+        }
+
+        const filename = path.join(__dirname, "./main.ejs");
+
+        ejs.renderFile(filename, data, options, function(err, str) {
+            if(err) {
+                console.error(err);
+            }
+
+            const outputFile = path.join(process.cwd(), out);
+            fs.ensureFileSync(outputFile);
+            fs.outputFileSync(outputFile, str);
+        })
     } catch(err) {
-        console.error(err)
+        console.error(err);
     }
 }
 
