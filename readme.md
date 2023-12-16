76fbec3cb77085f57897dc6516b294688cf972f2
Dean Longmire <deanlongmire100@gmail.com>
2023-12-15 23:15:47 -0500

    Add generation of cargo project

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # dean-code-generator 1.0.0
 
+[NPM Package](https://www.npmjs.com/package/dean-generator)
+
 ## Usage
 I only have one so far and it's very limited but I plan on expanding on this later
 
--- src/repos/cargo-rust/index.js
+++ src/repos/cargo-rust/index.js
@@ -1,11 +1,28 @@
 const path = require("path");
 const { generateGitignore } = require("./generators/gen-gitignore.js")
 const { generateReadme } = require("./generators/gen-readme.js")
+const { exec } = require('child_process');
 
 function generateCargoRust() {
-    console.log("Generating files...");
+    console.log("Initializing repo...");
     try {
         const repoName = path.basename(process.cwd());
+        const command = "cargo new " + repoName;
+
+        console.log("Generating cargo project...");
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`Error executing command: ${error.message}`);
+              return;
+            }
+
+            if (stderr) {
+                console.error(`Command stderr: ${stderr}`);
+                return;
+            }
+            
+              console.log(`Command output: ${stdout}`);
+        });
 
         generateGitignore(repoName);
         generateReadme(repoName);
